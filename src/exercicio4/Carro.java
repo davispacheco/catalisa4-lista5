afea48655c8f9da5939ba49502baa5a1d1484200
davispacheco <davipacheco1996@gmail.com>
2022-07-06 16:30:39 -0300

    Criar classeCarro, com os atributos quantidadeDePortas, modelo, marcae potencia(opcional, com o valor padrão 2.0). Esta é a resolução do exercício 4.

---
+++ src/exercicio4/Carro.java
@@ -0,0 +1,21 @@
+package src.exercicio4;
+
+public class Carro {
+    int quantidadeDePortas;
+    String modelo;
+    String marca;
+    double potencia;
+
+    public Carro(int quantidadeDePortas, String modelo, String marca) {
+        this.quantidadeDePortas = quantidadeDePortas;
+        this.modelo = modelo;
+        this.marca = marca;
+    }
+
+    public Carro(int quantidadeDePortas, String modelo, String marca, double potencia) {
+        this.quantidadeDePortas = quantidadeDePortas;
+        this.modelo = modelo;
+        this.marca = marca;
+        this.potencia = 2.0;
+    }
+}
