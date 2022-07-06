87fc1ead5ca69211621ba74c79f25831dced5184
davispacheco <davipacheco1996@gmail.com>
2022-07-06 18:35:19 -0300

    Criar classe Retangulo, com os atributos base e altura, bem como os métodos mudarValorDosLados, retornarValorDosLados, calcularArea e calcularPerimetro.

---
+++ src/exercicio7/Retangulo.java
@@ -0,0 +1,30 @@
+package src.exercicio7;
+
+public class Retangulo {
+    Double base;
+    double altura;
+
+    public Retangulo(Double base, double altura) {
+        this.base = base;
+        this.altura = altura;
+    }
+
+    public void mudarValorDosLados(double novaBase, double novaAltura) {
+        base = novaBase;
+        altura = novaAltura;
+    }
+
+    public String retornarValorDosLados() {
+        String valorDosLados = "Valor da base: " + base + ".\nValor da altura: " + altura + ".";
+        return valorDosLados;
+    }
+
+    public double calcularArea(double base, double altura) {
+        double areaDoRetangulo = base * altura;
+        return areaDoRetangulo;
+    }
+    public double calcularPerimetro(double base, double altura) {
+double perimetroDoRetangulo = (base + altura) * 2;
+return perimetroDoRetangulo;
+    }
+}
