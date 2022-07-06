82a3ad1361c8b1a7265aa98290f7077ef89e2ffb
davispacheco <davipacheco1996@gmail.com>
2022-07-06 18:10:11 -0300

    Criar classe Main, com a execução dos métodos presentes na classe Quadrado, cuja instância foi realizada para criar o objeto quadrado. Esta é a resolução do exercício 6.

---
+++ src/exercicio6/Main.java
@@ -0,0 +1,44 @@
+package src.exercicio6;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean repeticaoMenu = true;
+        Scanner input = new Scanner(System.in);
+        Quadrado quadrado = new Quadrado(10);
+        while (repeticaoMenu) {
+            System.out.println("O que deseja fazer?");
+            System.out.println("1: Mostrar valor do lado do quadrado");
+            System.out.println("2: Alterar valor do lado do quadrado");
+            System.out.println("3: Calcular a área do quadrado");
+            System.out.println("4: Finalizar");
+            String opcao = input.next();
+            switch (opcao) {
+                case "1":
+                    System.out.println("O valor atual do lado do quadrado é:" + quadrado.retornarValorDoLado());
+                    repeticaoMenu = true;
+                    break;
+                case "2":
+                    System.out.println("Informe o valor que deseja definir como lado do quadrado:");
+                    double ladoDoQuadrado = input.nextDouble();
+                    quadrado.mudarValorDoLado(ladoDoQuadrado);
+                    System.out.println("Lado do quadrado alterado com sucesso!");
+                    repeticaoMenu = true;
+                    break;
+                case "3":
+                    double areaDoQuadrado = quadrado.calcularArea(quadrado.tamanhoDoLado);
+                    System.out.println("A área do quadrado, cujo lado é " + quadrado.tamanhoDoLado + " é: " + areaDoQuadrado);
+                    repeticaoMenu = true;
+                    break;
+                case "4":
+                    System.out.println("Fim do programa!");
+                    repeticaoMenu = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida!");
+            }
+        }
+        input.close();
+    }
+}
