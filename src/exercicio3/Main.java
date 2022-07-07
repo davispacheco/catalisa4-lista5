0d3d547283c8a975055ea0e3481413246c2d393d
davispacheco <davipacheco1996@gmail.com>
2022-07-07 01:08:35 -0300

    Melhorar validações na classe Maim do pacote exercicio3, a fim de promover uma interação mais eficiente e evitar que o programa finalize antes de o usuário especificar o que deseja.

--- src/exercicio3/Main.java
+++ src/exercicio3/Main.java
@@ -22,24 +22,37 @@ public class Main {
                     repeticaoMenu = true;
                     break;
                 case "2":
-                    System.out.println("Quanto deseja depositar?");
-                    double valorDeposito = input.nextDouble();
-                    if (valorDeposito <= 0) {
-                        System.out.println("Não pode depositar valor menor ou igual a 0.");
-                    } else {
-                        contaCorrente.depositoConta(valorDeposito);
-                        System.out.println("Depósito realizado com sucesso!");
+                    boolean repeticaoDepositar = true;
+                    while (repeticaoDepositar) {
+                        System.out.println("Quanto deseja depositar?");
+                        double valorDeposito = input.nextDouble();
+                        if (valorDeposito <= 0) {
+                            System.out.println("Não pode depositar valor menor ou igual a 0.");
+                            repeticaoDepositar = true;
+                        } else {
+                            contaCorrente.depositoConta(valorDeposito);
+                            System.out.println("Depósito realizado com sucesso!");
+                            repeticaoDepositar = false;
+                        }
                     }
                     repeticaoMenu = true;
                     break;
                 case "3":
-                    System.out.println("Quanto deseja sacar?");
-                    double valorSaque = input.nextDouble();
-                    if (valorSaque > contaCorrente.saldo) {
-                        System.out.println("Não pode sacar um valor maior do que possui na conta.");
-                    } else {
-                        contaCorrente.saqueConta(valorSaque);
-                        System.out.println("Saque realizado com sucesso!");
+                    boolean repeticaoSacar = true;
+                    while (repeticaoSacar) {
+                        System.out.println("Quanto deseja sacar?");
+                        double valorSaque = input.nextDouble();
+                        if (valorSaque <= 0) {
+                            System.out.println("Não pode sacar um valor menor ou igual a 0.");
+                            repeticaoSacar = true;
+                        } else if (valorSaque > contaCorrente.saldo) {
+                            System.out.println("Não pode sacar um valor maior do que possui na conta.");
+                            repeticaoSacar = true;
+                        } else {
+                            contaCorrente.saqueConta(valorSaque);
+                            System.out.println("Saque realizado com sucesso!");
+                            repeticaoSacar = false;
+                        }
                     }
                     repeticaoMenu = true;
                     break;
